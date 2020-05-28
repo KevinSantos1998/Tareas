3f3da8d8aeee292c8fb736e0242730af3e60d9b4
KevinSantos1998 <41806832+KevinSantos1998@users.noreply.github.com>
2020-05-28 14:15:42 -0600

    Update main.cpp

--- ExamenPIII/Examen/Examen/main.cpp
+++ ExamenPIII/Examen/Examen/main.cpp
@@ -9,7 +9,19 @@ int main ()
 	Conjunto lol("1964700000");
 	Conjunto lol2("7589600000");
 	Conjunto lol3;
+	lol3 = lol + lol2;
+	cout << lol3<<"\n";
 	lol3 = lol & lol2;
-	cout << lol3;
+	cout << lol3 << "\n";
+	lol3 = lol - lol2;
+	cout << lol3 << "\n";
+	if(lol==lol2)
+	{
+		cout << "los conjuntos son iguales";
+	}
+	else
+	{
+		cout << "los conjuntos son diferentes";
+	}
 	_getch();
 }
