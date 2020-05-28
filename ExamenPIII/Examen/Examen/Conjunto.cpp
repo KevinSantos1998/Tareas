766b2754123de263f09584151f09d8086f62deac
KevinSantos1998 <41806832+KevinSantos1998@users.noreply.github.com>
2020-05-28 14:14:58 -0600

    Update Conjunto.cpp

--- ExamenPIII/Examen/Examen/Conjunto.cpp
+++ ExamenPIII/Examen/Examen/Conjunto.cpp
@@ -23,6 +23,7 @@ std::ostream& operator<<(std::ostream& out, const Conjunto& _num)
 
 	for (int i = 0; i < 10; i++)
 	{
+		if(_num.num[i]!=0)
 		out << _num.num[i];
 	}
 
@@ -79,4 +80,64 @@ Conjunto operator&(const Conjunto& c1, const Conjunto& c2)
 		}
 	}
 	return resultado;
+}
+
+Conjunto operator-(const Conjunto& c1, const Conjunto& c2)
+{
+	Conjunto resultado;
+	resultado = c1 + c2;
+	Conjunto un;
+	un = c1 & c2;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int h = 0; h < 10; h++)
+		{
+			if (resultado.num[i] == un.num[h]) 
+			{
+				resultado.num[i] = 0;
+			}
+		}
+	}
+	return resultado;
+}
+
+bool operator==(const Conjunto& c1, const Conjunto& c2)
+{
+	bool verdadero[10];
+	for (int i = 0; i < 10; i++)
+	{
+		verdadero[i] = false;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (c1.num[i] == c2.num[1])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[2])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[3])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[4])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[5])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[6])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[7])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[8])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[9])
+			verdadero[i] == true;
+		if (c1.num[i] == c2.num[0])
+			verdadero[i] == true;
+	}
+	if(verdadero[0]==true&& verdadero[1] == true && verdadero[2] == true && verdadero[3] == true && verdadero[4] == true && verdadero[5] == true && verdadero[6] == true && verdadero[7] == true &&
+		verdadero[8] == true && verdadero[9] == true)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
