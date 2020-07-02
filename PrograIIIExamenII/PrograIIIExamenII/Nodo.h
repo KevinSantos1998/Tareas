071ef428cd341ab24dd2eb7129408450ae8b473e
KevinSantos1998 <41806832+KevinSantos1998@users.noreply.github.com>
2020-07-02 12:42:43 -0600

    Update Nodo.h

--- PrograIIIExamenII/PrograIIIExamenII/Nodo.h
+++ PrograIIIExamenII/PrograIIIExamenII/Nodo.h
@@ -4,22 +4,30 @@
 #ifndef NODO_H
 #define NODO_H
 
+struct NodoReguistro
+{
+	int Codigo;
+	char nombre[20];
+	Nodo* siguiente;
+	Nodo* anterior;
+
+};
+
 class Nodo 
 {
 private:
-	int valor;
-	Nodo* siguiente;
-	Nodo* anterior;
 
 public:
 	Nodo(void);
 	//Valor del nodo, ptrAnterior, ptrSiguiente
-	Nodo(int, Nodo*, Nodo*);
+	Nodo(int,char[], Nodo*, Nodo*);
 
+	void setNombre(char[20]);
 	void setValor(int);
 	void setSiguiente(Nodo*);
 	void setAnterior(Nodo*);
 
+	char* getNombre();
 	int getValor();
 	Nodo* getAnterior();
 	Nodo* getSiguiente();
