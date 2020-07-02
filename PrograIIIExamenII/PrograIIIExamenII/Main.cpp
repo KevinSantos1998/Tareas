de3965489669a747caf26945e9a7293395c15251
KevinSantos1998 <41806832+KevinSantos1998@users.noreply.github.com>
2020-07-02 13:31:23 -0600

    Update Main.cpp

--- PrograIIIExamenII/PrograIIIExamenII/Main.cpp
+++ PrograIIIExamenII/PrograIIIExamenII/Main.cpp
@@ -1,40 +1,29 @@
 #include <iostream> 
 #include <deque> 
+#include "ListaDoble.h"
 
 using namespace std;
 
-void showdq(deque <int> g)
-{
-    deque <int> ::iterator it;
-    for (it = g.begin(); it != g.end(); ++it)
-        cout << '\t' << *it;
-    cout << '\n';
-}
-
 int main()
 {
-    deque <int> gquiz;
-    gquiz.push_back(10);
-    gquiz.push_front(20);
-    gquiz.push_back(30);
-    gquiz.push_front(15);
-    cout << "The deque gquiz is : ";
-    showdq(gquiz);
 
-    cout << "\ngquiz.size() : " << gquiz.size();
-    cout << "\ngquiz.max_size() : " << gquiz.max_size();
+    ListaDoble hola;
 
-    cout << "\ngquiz.at(2) : " << gquiz.at(2);
-    cout << "\ngquiz.front() : " << gquiz.front();
-    cout << "\ngquiz.back() : " << gquiz.back();
+    hola.agregarElementoFinal(20, "Kevin");
+    hola.agregarElementoInicio(22, "carmen");
+    hola.agregarElementoFinal(23, "Karla");
+    hola.agregarElementoInicio(24, "Maria");
+    hola.imprimirLista();
+    hola.estaLlena();
+    hola.estaVacia();
+    hola.ObtenerFinal();
+    hola.ObtenerInicio();
+    hola.PopFinal();
+    hola.PopInicio();
+    hola.imprimirLista();
+    hola.Guardarlista();
+    
 
-    cout << "\ngquiz.pop_front() : ";
-    gquiz.pop_front();
-    showdq(gquiz);
-
-    cout << "\ngquiz.pop_back() : ";
-    gquiz.pop_back();
-    showdq(gquiz);
 
     return 0;
 }
